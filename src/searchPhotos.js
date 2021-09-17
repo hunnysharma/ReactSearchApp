482b92ba32b2b43295cd98712f10a13fd7f761f1
Himanshu Sharma <32563057+hunnysharma@users.noreply.github.com>
2021-09-17 11:06:59 +0530

    Update searchPhotos.js

--- src/searchPhotos.js
+++ src/searchPhotos.js
@@ -5,6 +5,60 @@ import Unsplash, { toJson } from "unsplash-js";
 const unsplash = new Unsplash({
   accessKey: "P68afyGqAaZKMs9DFKgHW0uAoiI0qAWdTvPgYFTz6Fc",
 });
+function SearchPhotosMore() {
+  console.log('hiSug');
+  const [query, setQuery] = useState("");
+  const [pics, setPics] = useState([]);
+  const searchPhotos = async (e) => {
+    e.preventDefault();
+   
+
+    unsplash.search
+    .photos(query, 5, 30)
+    .then(toJson)
+    .then((json) => {
+      setPics(json.results);
+   });
+
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={searchPhotos}>
+        <label className="label" htmlFor="query">
+          {" "}
+          📷
+        </label>
+        <input
+          type="text"
+          name="query"
+          className="input"
+          placeholder={`Try "dog" or "apple"`}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit" className="button">
+          Search
+        </button>
+      </form>
+      <div className="card-list">
+      {pics.map((pic) => <div className="card" key={pic.id}>
+          
+      <img
+                className="card--image"
+                alt={pic.alt_description}
+                src={pic.urls.full}
+                width="40%"
+                height="40%"
+              ></img> 
+
+      </div>) }
+      </div>
+<button class="load_more" onClick={SearchPhotosMore}>Load More</button>
+    </>
+  );
+}
+
 
 export default function SearchPhotos() {
   console.log('hiSug');
